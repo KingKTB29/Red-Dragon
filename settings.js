dfa2abb697ddd9267bb3b32e278d8d75df6ac9b7
KingKTB29 <106971075+KingKTB29@users.noreply.github.com>
2022-06-26 21:02:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,29 +20,29 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is KTB🔥
+My Creator is KingKTB😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94781554407'] //Owner number in aive msg
+global.premium = ['+94781554407'] //Owner Number info
+global.ownernomer = '+94781554407' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = 'KTB' //Owner Name
+global.botname = ' Dark KTB' //Bot Name
 
-global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
-global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
+global.button = 'Hello' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://youtube.com/channel/UC91eH-zukJgszqroFFPEqEQ //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
-global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
+global.packname = 'KTB' //Bot Pacage Name
+global.author = 'KTB' //Author Name
+global.prefa = ['.alive','!','.','😂','❤','Fuck'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/KingKTB29' //Github Account!
 global.footer = '©_Kumuthu.' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
